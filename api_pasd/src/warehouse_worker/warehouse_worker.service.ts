0d238ad670b9e611f6c5fb1395e636a87380e335
tarcalas <tarcalibarnabas@gmail.com>
2023-01-15 15:26:10 +0100

    commit2

--- api_pasd/src/warehouse_worker/warehouse_worker.service.ts
+++ api_pasd/src/warehouse_worker/warehouse_worker.service.ts
@@ -7,6 +7,8 @@ export class WarehouseWorkerService {
     
     async updateLabel(){
         const fs = require('fs');
+        console.log(__dirname);
+        console.log(fs.readdirSync(__dirname));
         const fileDescriptor = fs.openSync('./newfile.txt', 'r');
         const file = new fs.File(fileDescriptor, './newfile.txt');
         const formData = new FormData();
